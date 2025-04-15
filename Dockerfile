02ab0b94eee0bd386fcbfa0875964c7d817ceb3f
Deyanvkc <deyan.vukic@epitech.eu>
2025-04-15 15:42:24 +0200

    test push for repo epitech

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,9 @@
-# Dockerfile pour l'image de Jenkins
+# Image de jenkins
 FROM jenkins/jenkins:lts-jdk17
-# On fait une copy et on run les plugins
-COPY --chown=jenkins:jenkins plugins.txt /usr/share/jenkins/ref/plugins.txt
+
+# Copie du fichier plugins.txt et installation des plugins
+COPY plugins.txt /usr/share/jenkins/ref/plugins.txt
 RUN jenkins-plugin-cli -f /usr/share/jenkins/ref/plugins.txt
+
+# Création du dossier pour la config JCasC
+RUN mkdir -p /var/jenkins_home/casc_configs && chown -R jenkins:jenkins /var/jenkins_home
--- job_dsl.groovy
+++ job_dsl.groovy
@@ -1,21 +1,50 @@
-// Ici les jobs
-
-pipelineJob('job-dsl-plugin') {
-    definition {
-        cpsScm {
-            scm {
-                git {
-                    remote {
-                        url('https://github.com/Deyanvkc/MyMarvinBootstrap#')
-                    }
-                    branch('*/main')
-                }
-            }
-            lightweight()
-        }
+// Job -> clone-repository
+job('Tools/clone-repository') {
+    description('Clone a git repo with a url given as parameter')
+    parameters {
+        stringParam('GIT_REPOSITORY_URL', '', 'Git URL of the repository to clone')
     }
+    triggers {
+        // Aucun trigger, job exécuté manuellement
+    }
+    steps {
+        // Nettoyage du workspace avant chaque exécution
+        shell('echo "Cleaning workspace" && rm -rf *')
+
+        // Clone le dépôt Git
+        shell('git clone ${GIT_REPOSITORY_URL}')
+    }
+    //disabled(false)  // Peut être pas nécessaire tant que pas de triggers spécifiés
 }
 
-job{
-    name("deploy-application")
+// Job -> SEED - Crée d'autres jobs dynamiquement
+job('Tools/SEED') {
+    description("Job that generates jobs from the given input")
+    parameters {
+        stringParam('GITHUB_NAME', '', 'GitHub repository owner/repo_name')
+        stringParam('DISPLAY_NAME', '', 'Display name for the job')
+    }
+    steps {
+        dsl {
+            // Script DSL pour créer un job
+            """
+            job("\${DISPLAY_NAME}") {
+                description('Job généré dynamiquement par le SEED')
+                scm {
+                    git("\${GITHUB_NAME}")
+                }
+                triggers {
+                    scm('H/1 * * * *')  // Vérification des changements toutes les minutes
+                }
+                steps {
+                    shell('make fclean')
+                    shell('make')
+                    shell('make tests_run')
+                    shell('make clean')
+                }
+            }
+            """
+        }
+    }
+    //disabled(false)  // Peut être pas nécessaire tant que pas de triggers spécifiés
 }
